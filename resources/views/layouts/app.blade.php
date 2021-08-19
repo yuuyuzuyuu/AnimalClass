71cc7cc4a66d0deed96912bf5c3aac9590b28ee5
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-08-19 07:57:26 +0000

    [Add] page-link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <title>animal class</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{asset('/assets/css/app.css')}}">
     </head>
   
     <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,38 @@
     <div class="text-center">
         <h1>Welcome</h1>
     </div>
+    
+    <ul id="page-link">
+        <li><a href="#top">TOP</a></li>
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#dogandcat">Dog and Cat</a></li>
+        <li><a href="#reservation">RESERVATION</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+        <li><a href="#sns">SNS</a></li>
+    </ul>
+            
+    <section id="top">
+        <h2>内容</h2>
+    </section>
+            
+    <section id="about">
+        <h2>内容</h2>
+    </section>
+            
+    <section id="dogandcat">
+        <h2>内容</h2>
+    </section>
+            
+    <section id="reservation">
+        <h2>内容</h2>
+    </section>
+            
+    <section id="contact">
+        <h2>内容</h2>
+    </section>
+            
+    <section id="sns">
+        <h2>内容</h2>
+    </section>
 
 @endsection
