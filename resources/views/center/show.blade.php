7527b617978ea74edb66df49078986e3412ff01f
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-09-07 06:09:24 +0000

    [Fix] layout

--- resources/views/center/show.blade.php
+++ resources/views/center/show.blade.php
@@ -2,53 +2,57 @@
 
 @section('content')
 <div class="mypage">
-    <p class="second-title"><i class="fas fa-paw"></i>{{ $center -> name }}</p>
-    <table class="table table-borderless">
-        <tr>
-            <th>電話番号</th>
-            <td>{{ $center -> tel }}</td>
-        </tr>
-        <tr>
-            <th>郵便番号</th>
-            <td>〒{{ $center -> postcode }}</td>
-        </tr>
-        <tr>
-            <th>都道府県</th>
-            <td>{{ $center -> PrefName }}</td>
-        </tr>
-        <tr>
-            <th>都道府県以下住所</th>
-            <td>{{ $center -> address }}</td>
-        </tr>
-        <tr>
-            <th>SNS</th>
-            <td>
-                @if ($center->homepage)
-                    <a href="{{ $center->homepage }}"><i class="fas fa-home fa-2x"></i></a>
-                @endif
-                @if ($center->instagram)
-                    <a href="{{ $center->instagram }}"><i class="fab fa-instagram fa-2x"></i></a>
-                @endif
-                @if ($center->twitter)
-                    <a href="{{ $center->twitter }}"><i class="fab fa-twitter fa-2x"></i></a>
-                @endif
-                @if ($center->facebook)
-                    <a href="{{ $center->facebook }}"><i class="fab fa-facebook fa-2x"></i></a>
-                @endif
-            </td>
-        </tr>
-    </table>
-    @if (Auth::guard('center')->check())
-        @if(Auth::id() === $center->id)
-            {!! link_to_route('center.edit', '編集', ['center' => $center->id], ['class' => 'btn btn-block']) !!}
+    <div class="mypage-content">
+        <p class="second-title"><i class="fas fa-paw"></i>{{ $center -> name }}</p>
+        <table class="table table-borderless">
+            <tr>
+                <th>電話番号</th>
+                <td>{{ $center -> tel }}</td>
+            </tr>
+            <tr>
+                <th>郵便番号</th>
+                <td>〒{{ $center -> postcode }}</td>
+            </tr>
+            <tr>
+                <th>都道府県</th>
+                <td>{{ $center -> PrefName }}</td>
+            </tr>
+            <tr>
+                <th>都道府県以下住所</th>
+                <td>{{ $center -> address }}</td>
+            </tr>
+            <tr>
+                <th>SNS</th>
+                <td>
+                    @if ($center->homepage)
+                        <a href="{{ $center->homepage }}"><i class="fas fa-home fa-2x"></i></a>
+                    @endif
+                    @if ($center->instagram)
+                        <a href="{{ $center->instagram }}"><i class="fab fa-instagram fa-2x"></i></a>
+                    @endif
+                    @if ($center->twitter)
+                        <a href="{{ $center->twitter }}"><i class="fab fa-twitter fa-2x"></i></a>
+                    @endif
+                    @if ($center->facebook)
+                        <a href="{{ $center->facebook }}"><i class="fab fa-facebook fa-2x"></i></a>
+                    @endif
+                </td>
+            </tr>
+        </table>
+        @if (Auth::guard('center')->check())
+            @if(Auth::id() === $center->id)
+                {!! link_to_route('center.edit', '編集', ['center' => $center->id], ['class' => 'btn btn-block']) !!}
+            @endif
         @endif
-    @endif
-
-    <div class="second-title"><i class="fas fa-gift"></i>この施設のわんにゃん</div>
-    
-    <div class="animal-index">
-        @include('user.animal.box')
-        {{ $animals->links() }}
     </div>
+    
+    <div class="mypage-content">
+        <div class="second-title"><i class="fas fa-gift"></i>この施設のわんにゃん</div>
+        <div class="animal-index">
+            @include('user.animal.box')
+            {{ $animals->links() }}
+        </div>
+    </div>
+
 </div>
 @endsection
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -2,34 +2,37 @@
 
 @section('content')
 <div class="mypage">
-    <div class="second-title"><i class="fas fa-paw"></i>マイページ</div>
-    <table class="table table-borderless">
-        <tr>
-            <th>なまえ</th>
-            <td>{{ $user -> name }}</td>
-        </tr>
-        <tr>
-            <th>ニックネーム</th>
-            <td>{{ $user -> nickname }}</td>
-        </tr>
-        <tr>
-            <th>メールアドレス</th>
-            <td>{{ $user -> email }}</td>
-        </tr>
-        <tr>
-            <th>電話番号</th>
-            <td>{{ $user -> tel }}</td>
-        </tr>
-        <tr>
-            <th>居住都道府県</th>
-            <td>{{ $user -> prefName }}</td>
-        </tr>
-    </table>
+    <div class="mypage-content">
+        <div class="second-title"><i class="fas fa-paw"></i>マイページ</div>
+        <table class="table table-borderless">
+            <tr>
+                <th>なまえ</th>
+                <td>{{ $user -> name }}</td>
+            </tr>
+            <tr>
+                <th>ニックネーム</th>
+                <td>{{ $user -> nickname }}</td>
+            </tr>
+            <tr>
+                <th>メールアドレス</th>
+                <td>{{ $user -> email }}</td>
+            </tr>
+            <tr>
+                <th>電話番号</th>
+                <td>{{ $user -> tel }}</td>
+            </tr>
+            <tr>
+                <th>居住都道府県</th>
+                <td>{{ $user -> prefName }}</td>
+            </tr>
+        </table>
+        {!! link_to_route('user.edit', '編集', ['user' => $user->id], ['class' => 'btn btn-block']) !!}
+    </div>
 
-    {!! link_to_route('user.edit', '編集', ['user' => $user->id], ['class' => 'btn btn-block']) !!}
-
-    <div class="second-title"><i class="fas fa-heart"></i>お気に入りの子</div>
-    @include('user.animal.box')
+    <div class="mypage-content">
+        <div class="second-title"><i class="fas fa-heart"></i>お気に入りの子</div>
+        @include('user.animal.box')
+    </div>
 </div>
 
 @endsection
