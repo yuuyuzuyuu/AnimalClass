bc1354fcd8d0af5244e3b0a42542f02812282c3e
yuuyuzuyuu <ym.74.mn@gmail.com>
2022-03-21 11:45:56 +0000

    ナビゲーションバーレスポンシブ対応

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,17 @@
 function mediaQueriesWin(){
     var width = $(window).width();
-    $(".has-child>a").on('click', function() {
-       var parentElem = $(this).parent();
-       $(parentElem).toggleClass('active');
-       $(parentElem).children('ul').stop().slideToggle(500);
-       return false;
-    });
+    if(width<=768){
+        $(".has-child>a").on('click', function() {
+            var parentElem = $(this).parent();
+            $(parentElem).toggleClass('active');
+            $(parentElem).children('ul').stop().slideToggle(500);
+            return false;
+        });
+    }else{
+        $(".has-child>a").off('click');
+        $(".has-child>a").removeClass('active');
+        $('.has-child').children('ul').css("display", "");
+    }
 }
 
 $(window).resize(function() {
