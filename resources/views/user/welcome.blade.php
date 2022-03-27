75a9a73cd113de64beadca99170b96e545b8108d
yuuyuzuyuu <ym.74.mn@gmail.com>
2022-03-27 21:40:45 +0000

    トップレスポンシブ対応

--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -26,43 +26,53 @@
         </section>
 
         <section id="about">
-            <div class="top-title">ABOUT</div>
-            <p>あにまるくらすでは、<br>保護されたわんちゃんねこちゃんたちと<br>新しい家族をつなげる活動を行っています。</p>
-            <p>大切にしてくれる家族に出会えるのを、<br>みんな心待ちにしています。</p>
-            <p>どんな子がいるのか、<br>ちょっと覗いてみませんか？</p>
+            <div class="top-content">
+                <div class="top-title">ABOUT</div>
+                <p>あにまるくらすでは、<br>保護されたわんちゃんねこちゃんたちと<br>新しい家族をつなげる活動を行っています。</p>
+                <p>大切にしてくれる家族に出会えるのを、<br>みんな心待ちにしています。</p>
+                <p>どんな子がいるのか、<br>ちょっと覗いてみませんか？</p>
+            </div>
         </section>
 
         <section id="dogsandcats">
-            <div class="top-title">Dogs and Cats</div>
-                <p>現在、各施設でお預かりしている子たちです。</p>
-                <p>情報の更新は施設が逐一行っておりますが、<br>既に譲渡が決定してしまっている子がいる場合もありますので<br>ご了承ください。</p>
-                <div class="second-title"><i class="fas fa-star"></i>新着わんにゃん</div>
-                <div class="top-animal-index">
-                    @include('user.animal.box')
-                </div>
+            <div class="top-content">
+                <div class="top-title">Dogs and Cats</div>
+                    <p>現在、各施設でお預かりしている子たちです。</p>
+                    <p>情報の更新は施設が逐一行っておりますが、<br>既に譲渡が決定してしまっている子がいる場合もありますので<br>ご了承ください。</p>
+                    <div class="second-title"><i class="fas fa-star"></i>新着わんにゃん</div>
+                    <div class="top-animal-index">
+                        @include('user.animal.box')
+                    </div>
+            </div>
             <div class="text-right">{!! link_to_route('animals.index', 'もっと見る', [], ['class' => 'btn']) !!}</div>
         </section>
 
         <section id="reservation">
-            <div class="top-title">Reservation</div>
-            <p>直接会ってみたい方は、<br>こちらで予約を受け付けております。</p>
+            <div class="top-content">
+                <div class="top-title">Reservation</div>
+                <p>直接会ってみたい方は、<br>こちらで予約を受け付けております。</p>
+            </div>
         </section>
 
         <section id="contact">
-            <div class="top-title">Contact</div>
-            <p>このサイトについて<br>何かご不明な点がございましたら、<br>お気軽にお問い合わせください。</p>
-            <p>&#x203B;施設へのご質問は<br>直接施設へお問い合わせください。</p>
-            <p class="tel"><i class="fas fa-phone"></i>000-0000-0000</p>
-            {!! link_to_route('contact.index', 'お問い合わせフォーム', [], ['class' => 'btn']) !!}
+            <div class="top-content">
+                <div class="top-title">Contact</div>
+                <p>このサイトについて<br>何かご不明な点がございましたら、<br>お気軽にお問い合わせください。</p>
+                <p>&#x203B;施設へのご質問は<br>直接施設へお問い合わせください。</p>
+                <p class="tel"><i class="fas fa-phone"></i>000-0000-0000</p>
+                {!! link_to_route('contact.index', 'お問い合わせフォーム', [], ['class' => 'btn']) !!}
+            </div>
         </section>
 
         <section id="sns">
-            <div class="top-title">SNS</div>
-                <div class="icon">
-                    <a href="https://www.instagram.com/"><i class="fab fa-instagram fa-4x"></i></a>
-                    <a href="https://twitter.com/"><i class="fab fa-twitter fa-4x"></i></a>
-                    <a href="https://ja-jp.facebook.com/"><i class="fab fa-facebook fa-4x"></i></a>
-                </div>
+            <div class="top-content">
+                <div class="top-title">SNS</div>
+                    <div class="icon">
+                        <a href="https://www.instagram.com/"><i class="fab fa-instagram fa-4x"></i></a>
+                        <a href="https://twitter.com/"><i class="fab fa-twitter fa-4x"></i></a>
+                        <a href="https://ja-jp.facebook.com/"><i class="fab fa-facebook fa-4x"></i></a>
+                    </div>
+            </div>
         </section>
     </div>
 
